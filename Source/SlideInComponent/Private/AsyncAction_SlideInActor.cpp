981814ede96491dcc8b88d1e3d446031b4398cd6
Josie Thompson <dot.slash.josie@gmail.com>
2024-06-23 13:11:24 -0700

    removed slide in component in favor of slide in async action

---
+++ Source/SlideInComponent/Private/AsyncAction_SlideInActor.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright 2024 Josie Thompson, MIT License
+
+
+#include "AsyncAction_SlideInActor.h"
+
+#include "SlideInData.h"
+
+UAsyncAction_SlideInActor *
+UAsyncAction_SlideInActor::SlideInActor(const UObject * WorldContext, AActor * ActorToSlideIn,
+                                        FVector SlideInOffset, UCurveFloat * SlideInCurve, float PlaybackSpeed)
+{
+    auto* Action = NewObject<UAsyncAction_SlideInActor>();
+    Action->ActorToSlide = ActorToSlideIn;
+    Action->Timeline = NewObject<UTimelineComponent>();
+    Action->Timeline->RegisterComponentWithWorld(WorldContext->GetWorld());
+    
+    Action->OnUpdateTimeline.BindDynamic(Action, &ThisClass::HandleAnimationUpdate);
+    Action->Timeline->AddInterpFloat(SlideInCurve, Action->OnUpdateTimeline,
+                                     TEXT("Alpha"), TEXT("SlideIn"));
+    Action->Timeline->SetPlayRate(PlaybackSpeed);
+
+    Action->OnTimelineFinished.BindDynamic(Action, &ThisClass::HandleAnimationFinished);
+    Action->Timeline->SetTimelineFinishedFunc(Action->OnTimelineFinished);
+
+    Action->SlideToPosition = ActorToSlideIn->GetActorLocation();
+    Action->SlideFromPosition = Action->SlideToPosition + SlideInOffset;
+    return Action;
+}
+
+UAsyncAction_SlideInActor *
+UAsyncAction_SlideInActor::SlideInActorFromData(const UObject * WorldContext, AActor * ActorToSlideIn,
+                                                const USlideInData * SlideInData)
+{
+    return SlideInActor(WorldContext, ActorToSlideIn,
+                        SlideInData->SlideFromOffset, SlideInData->SlideInCurve,
+                        SlideInData->SlideInTime != 0.f? 1.f/SlideInData->SlideInTime : 0.f);
+}
+
+void UAsyncAction_SlideInActor::Activate()
+{
+    Timeline->PlayFromStart();
+}
+
+void UAsyncAction_SlideInActor::HandleAnimationUpdate(float Alpha)
+{
+    const auto NewLocation = FMath::Lerp(SlideFromPosition, SlideToPosition, Alpha);
+    ActorToSlide->SetActorRelativeLocation(NewLocation);
+}
+
+void UAsyncAction_SlideInActor::HandleAnimationFinished()
+{
+    OnFinished.Broadcast();
+}
---
+++ Source/SlideInComponent/Public/AsyncAction_SlideInActor.h
@@ -0,0 +1,68 @@
+﻿// Copyright 2024 Josie Thompson, MIT License
+
+#pragma once
+#include "Components/TimelineComponent.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncAction_SlideInActor.generated.h"
+
+class USlideInData;
+/**
+ * UAsyncAction_SlideInActor
+ *  Play an animation that slides an actor in from another position
+ */
+UCLASS()
+class SLIDEINCOMPONENT_API UAsyncAction_SlideInActor : public UBlueprintAsyncActionBase {
+    GENERATED_BODY()
+public:
+    UFUNCTION(BlueprintCallable, Category="Actor Transitions", meta=(WorldContext="WorldContext"))
+    static UAsyncAction_SlideInActor *
+    SlideInActor(const UObject* WorldContext, AActor* ActorToSlideIn, FVector SlideInOffset,
+                 UCurveFloat* SlideInCurve, float PlaybackSpeed);
+
+    UFUNCTION(BlueprintCallable, Category="Actor Transitions", meta=(WorldContext="WorldContext"))
+    static UAsyncAction_SlideInActor *
+    SlideInActorFromData(const UObject * WorldContext, AActor * ActorToSlideIn, const USlideInData* SlideInData);
+
+    // Async Action interface
+    virtual void Activate() override;
+    
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationFinished);
+
+    /** Do something when the animation finishes */
+    UPROPERTY(BlueprintAssignable)
+    FOnAnimationFinished OnFinished;
+protected:
+
+    /** Handle what happens when the slide-in animation finishes */
+    UFUNCTION()
+    virtual void HandleAnimationFinished();
+
+    /** Handle what happens when the slide-in animation updates */
+    UFUNCTION()
+    virtual void HandleAnimationUpdate(float Alpha);
+
+    /** The cached actor to move */
+    UPROPERTY(Transient)
+    AActor* ActorToSlide;
+    
+    /** The cached actor position to slide from */
+    UPROPERTY(Transient)
+    FVector SlideFromPosition;
+
+    /** The cached actor position to slide to */
+    UPROPERTY(Transient)
+    FVector SlideToPosition;
+    
+private:
+    /** The timeline that animates the alpha lerping variable */
+    UPROPERTY(Transient)
+    TObjectPtr<UTimelineComponent> Timeline;
+
+    /** Delegate handle to bind UpdatePosition with */
+    UPROPERTY(Transient)
+    FOnTimelineFloat OnUpdateTimeline;
+
+    /** Delegate handle to bind HandleTimelineFinished */
+    UPROPERTY(Transient)
+    FOnTimelineEvent OnTimelineFinished;
+};
